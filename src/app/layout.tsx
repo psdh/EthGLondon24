a48381e6604c80a27b1d3f39315afe3c0bbf49df
Sodhi <prabhjyotsingh95@gmail.com>
2024-03-16 13:04:22 +0000

    Initial commit

---
+++ src/app/layout.tsx
@@ -0,0 +1,23 @@
+import '@rainbow-me/rainbowkit/styles.css'
+import { Metadata } from 'next'
+
+import { ClientProviders } from '@/lib/providers'
+
+export const metadata: Metadata = {
+  title: 'ENS Frontend Template',
+  description: 'Starter web app for web3 developers',
+}
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <html lang="en">
+      <body>
+        <ClientProviders>{children}</ClientProviders>
+      </body>
+    </html>
+  )
+}
